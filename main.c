e72bae0334e299b468d8149eb966ea649003a432
Watashiku <adelbeque@hotmail.fr>
2016-05-09 19:26:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,38 @@
 
 int main()
 {
-    DB database=creation("graphe1.csv");
-    lectureDB(database);
+    int depart = -1, arrivee = -1, continuer = 1;
+    DB database=creation("metro.csv");
+
+    while(continuer){
+        lectureDB(database);
+
+        printf("\n\n\n\n\n");
+
+        printf("CHOISSISSEZ VOTRE STATION DE DEPART (numero) : \n\n\n\n");
+        scanf("%d", &depart);
+        while(depart<0 || depart > database.g.nbSommets){
+            scanf("%d", &depart);
+        }
+
+        printf("\n\n\n\n\n");
+
+        printf("CHOISSISSEZ VOTRE STATION D'ARRIVEE (numero) : \n\n\n\n");
+        scanf("%d", &arrivee);
+        while(arrivee < 0 || arrivee > database.g.nbSommets){
+            scanf("%d", &arrivee);
+        }
+
+        cheminPlusCourt(database, depart, arrivee);
+
+        printf("RECHERCHER UN AUTRE ITINERAIRE ? (1 oui  0 non)");
+
+        scanf("%d", &continuer);
+        while(continuer != 0 || continuer != 1){
+            printf("J'espere que tu es content de toi !\n");
+            scanf("%d", &continuer);
+        }
+
+    }
     return 0;
 }
